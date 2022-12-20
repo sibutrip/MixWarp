c58c606201f01cd919e1018f987ddaa2014d7511
Cory Tripathy <ctripathy0@gmail.com>
2022-12-20 09:00:15 -0500

    backend: record, loop, speed control frontend: record toggle, loop toggle, speed control fade

--- PdProject/ContentView.swift
+++ PdProject/ContentView.swift
@@ -15,14 +15,15 @@ class PdEngine {
     }
     init() {
         pd = PdAudioController()
-        pd?.configureAmbient(withSampleRate: 48000, outputChannels: 2)
+        pd?.configurePlayback(withSampleRate: 48000, inputChannels: 2, outputChannels: 2, inputEnabled: true)
+        //        pd?.configureAmbient(withSampleRate: 48000, outputChannels: 2)
         pd?.isActive = true
     }
 }
 
 class Patch: ObservableObject {
     var isActive = false
-    var patch = PDPatch(file: "test.pd")
+    var patch = PDPatch(file: "audioTest.pd")
     var instanceNumber: Float = 1
     var toggleOnOff = false
     @Published var instanceDict = [Float: Bool]()
@@ -46,12 +47,41 @@ class Patch: ObservableObject {
         patch?.instanceCount(instanceNumber, toggle: currentInstanceToggleFloat)
     }
     
+    func toggleRecord(onOff: Bool) {
+        let onOffFloat: Float = onOff ? 1 : 0
+        print("toggle record")
+        patch?.toggleRecord(onOffFloat)
+    }
+    
+    func toggleLoop(onOff: Bool) {
+        let onOffFloat: Float = onOff ? 1 : 0
+        print("toggle loop")
+        patch?.toggleLoop(onOffFloat)
+    }
+    
+    func floatFader(floatValue: Float) {
+        patch?.floatFader(floatValue)
+    }
+    
     func toggleIsGoofy(instanceNumber: Float) {
         goofyDict[instanceNumber]?.toggle()
         let isGoofyToggleFloat = dictToFloat(dict: goofyDict, key: instanceNumber)
         print("(\(Int(instanceNumber)),\(String(isGoofyToggleFloat)))")
         patch?.instanceIsGoofy(instanceNumber, toggle: isGoofyToggleFloat)
     }
+    
+//    func sendBang() {
+//        patch?.sendBang()
+//    }
+    
+    func sendBangToRecord() {
+        patch?.sendBangToRecord()
+    }
+    
+    func sendBangToLoop() {
+        patch?.sendBangToLoop()
+    }
+
     
     init() {
         for i in 1..<6 {
@@ -63,48 +93,32 @@ class Patch: ObservableObject {
 
 struct ContentView: View {
     var pd = PdEngine()
-    @StateObject var patch = Patch()
+    @StateObject var vm = ViewModel()
     
     var body: some View {
-        VStack {
-            HStack {
-                Text("on/off")
-                ForEach(1..<6, id: \.self) { instance in
-                    Button {
-                        patch.toggleInstance(instanceNumber: Float(instance))
-                    } label: {
-                        RoundedRectangle(cornerRadius: 15)
-                            .foregroundColor(.mint)
-                            .frame(width: 50, height: 50)
-                            .overlay {
-                                Text(patch.instanceDict[Float(instance)] ?? true ? "X" : "")
-                                    .foregroundColor(.black)
-                            }
-                    }
-                }
+        HStack {
+            Spacer()
+            VStack {
+                Bang(isOn: $vm.bang1IsOn)
+                Text("Record is \(vm.bang1IsOn ? "ON" : "off")")
+                    .frame(width: 120)
             }
-            HStack {
-                Text("goofy\ntoggle")
-                ForEach(1..<6, id: \.self) { instance in
-                    Button {
-                        patch.toggleIsGoofy(instanceNumber: Float(instance))
-                    } label: {
-                        RoundedRectangle(cornerRadius: 15)
-                            .foregroundColor(.mint)
-                            .frame(width: 50, height: 50)
-                            .overlay {
-                                Text(patch.goofyDict[Float(instance)] ?? true ? "X" : "")
-                                    .foregroundColor(.black)
-                            }
-                    }
-                }
+            VStack {
+                Bang(isOn: $vm.bang2IsOn)
+                Text("Loop is \(vm.bang2IsOn ? "ON" : "off")")
+                    .frame(width: 120)
+            }
+            VStack {
+                Fader(value: $vm.fader1Value)
+                    .frame(height: 150.0)
+                Text("\(vm.fader1Value)")
             }
         }
     }
 }
 
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
